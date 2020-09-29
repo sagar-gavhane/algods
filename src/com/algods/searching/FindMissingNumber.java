5143163351ad66aac8483b75309d1330b23611aa
Sagar Gavhane <sgavhane70@gmail.com>
2020-09-29 14:27:40 +0530

    XOR solution of find missing number

--- src/com/algods/searching/FindMissingNumber.java
+++ src/com/algods/searching/FindMissingNumber.java
@@ -11,4 +11,20 @@ public class FindMissingNumber {
 
     return sumByFormula - sumByNumber;
   }
+
+  // XOR technique
+  public static int solution2(int[] numbers) {
+    int x1 = numbers[0];
+    int x2 = 1;
+
+    for (int i = 1; i < numbers.length; i++)
+      x1 = x1 ^ numbers[i];
+
+
+    for (int i = 2; i <= numbers.length + 1; i++)
+      x2 = x2 ^ i;
+    
+
+    return x1 ^ x2;
+  }
 }
