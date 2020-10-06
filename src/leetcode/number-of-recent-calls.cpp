18674b9f6a9e8064f6182e58e38e6e20bef73bbd
Sagar <sgavhane70@gmail.com>
2020-10-06 23:34:49 +0530

    Create number-of-recent-calls.cpp

---
+++ src/leetcode/number-of-recent-calls.cpp
@@ -0,0 +1,20 @@
+// https://leetcode.com/problems/number-of-recent-calls/
+
+class RecentCounter {
+public:
+  queue<int> q;
+
+  RecentCounter() {
+    q = {};
+  }
+    
+  int ping(int t) {
+    q.push(t);
+    
+    while(q.front() < t - 3000) {
+      q.pop();
+    }
+    
+    return q.size();
+  }
+};
